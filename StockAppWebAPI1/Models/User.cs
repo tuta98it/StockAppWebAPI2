d26a6d7bbf4b8bf0d1a83a933786e1e1dc4a4919
Trần Anh Tú <tutran1998.tt@gmail.com>
2024-02-16 17:40:10 +0700

    [update] mapping user model with db

--- StockAppWebAPI1/Models/User.cs
+++ StockAppWebAPI1/Models/User.cs
@@ -1,31 +1,43 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace StockAppWebAPI1.Models
 {
+    [Table("users")]
     public class User
     {
+        [Key]
+        [Column("user_id")]
         public int UserId { get; set; }
 
         [Required(ErrorMessage = "Username is required")]
         [StringLength(100, MinimumLength = 6, ErrorMessage = "Username must be between 6 and 100 characters")]
+        [Column("username")]
         public string UserName { get; set; } = string.Empty;
 
 
         [Required(ErrorMessage = "Password is required")]
         [StringLength(200, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 200 characters")]
+        [Column("hashed_password")]
         public string HashedPassword { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Email is required")]
         [EmailAddress(ErrorMessage = "Invalid email required")]
+        [Column("email")]
         public string Email { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Phone number is required")]
         [RegularExpression(@"^\+?\d{10,15}$", ErrorMessage = "Invalid phone number")]
+        [Column("phone")]
         public string Phone { get; set; } = string.Empty;
 
         [StringLength(255, ErrorMessage = "Full name cannot exceed 255 characters")]
+        [Column("fullname")]
         public String? FullName { get; set; }
+        [Column("date_of_birth")]
         public DateTime? DateOfBirth { get; set; }
+
+        [Column("country")]
 
         public string? Country { get; set; }
 
