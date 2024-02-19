d947d5d448252f247f6cda124e340df3ba412e7e
tuta98it <tutran1998.tt@gmail.com>
2024-02-19 19:18:45 +0700

    update app config

--- StockAppWebAPI1/obj/Debug/net6.0/StockAppWebAPI1.AssemblyInfo.cs
+++ StockAppWebAPI1/obj/Debug/net6.0/StockAppWebAPI1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StockAppWebAPI1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf4728f4c4600260d3813a9e092376d0a6061395")]
 [assembly: System.Reflection.AssemblyProductAttribute("StockAppWebAPI1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StockAppWebAPI1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
