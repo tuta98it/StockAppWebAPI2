2df32520356bb1e9863ff30db66ed3cc3c093562
Trần Anh Tú <tutran1998.tt@gmail.com>
2024-04-08 17:23:33 +0700

    [feat] api Quote/ws

--- StockAppWebAPI1/Controllers/QuoteController.cs
+++ StockAppWebAPI1/Controllers/QuoteController.cs
@@ -5,9 +5,10 @@ using System.Net.WebSockets;
 using System.Text;
 using System.Text.Json;
 
-namespace StockAppWebAPI1.Controllers
+namespace StockAppWebApi.Controllers
 {
-    [Route("api/ws")]
+    [ApiController]
+    [Route("api/[controller]")]
     public class QuoteController : ControllerBase
     {
         private readonly IQuoteService _quoteService;
@@ -18,32 +19,39 @@ namespace StockAppWebAPI1.Controllers
         [HttpGet("ws")]
         //https://localhost:port/api/quote/ws
         public async Task GetRealtimeQuotes(
-            int page = 1,
-            int limit = 10,
+            int page = 1, int limit = 10,
             string sector = "",
-            string industry = "")
+            string industry = ""
+            )
         {
             if (HttpContext.WebSockets.IsWebSocketRequest)
             {
                 using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+                //await _webSocketManager.AddWebSocket(webSocket);
                 while (webSocket.State == WebSocketState.Open)
                 {
-
-                    List<RealtimeQuote>? quotes = await _quoteService.GetRealtimeQuotes(page, limit, sector, industry);
+                    List<RealtimeQuote>? quotes = await _quoteService
+                                                    .GetRealtimeQuotes(page, limit, sector, industry);
+                    //convert list of objects to json
                     string jsonString = JsonSerializer.Serialize(quotes);
                     var buffer = Encoding.UTF8.GetBytes(jsonString);
-                    await webSocket.SendAsync(
-                        new ArraySegment<byte>(buffer),
+                    await webSocket.SendAsync(new ArraySegment<byte>(buffer),
                         WebSocketMessageType.Text, true, CancellationToken.None);
+
                     await Task.Delay(2000); // Đợi 2 giây trước khi gửi giá trị tiếp theo
                 }
-                await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure,
-                            "Connection closed by the server", CancellationToken.None);
             }
             else
             {
                 HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
             }
         }
+        [HttpGet("historical")]
+        public async Task<IActionResult> GetHistoricalQuotes(int days, int stockId)
+        {
+            var historicalQuotes = await _quoteService.GetHistoricalQuotes(days, stockId);
+            return Ok(historicalQuotes);
+        }
     }
 }
+
